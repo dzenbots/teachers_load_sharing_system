87e00ef293aff25ac7c2ae244793aadce29f3499
DzenBots <vv.khritkin@gmail.com>
2020-05-05 12:35:21 +0300

    add 404 error page to main app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from .views import main
 
 
@@ -6,6 +6,10 @@ def create_app(config_file='settings.py'):
 
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
-    app.register_blueprint(main, url_prefix='')
+    app.register_blueprint(main)
+
+    @app.errorhandler(404)
+    def http_404_handler(error):
+        return render_template('404_error.html')
 
     return app
--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
-from flask import Blueprint
-from .models import initialize_db
+from flask import Blueprint, render_template
+from .models import db, initialize_db
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__,  static_folder="static", template_folder="templates")
 
 
 @main.before_request
 def before_request():
     initialize_db()
+
+
+@main.teardown_request
+def after_request(exception):
+    db.close()
 
 
 @main.route('/')
