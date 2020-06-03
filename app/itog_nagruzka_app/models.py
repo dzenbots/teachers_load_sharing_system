8bf538968ac072064a738dcd7e01700ae3e3808a
DzenBots <vv.khritkin@gmail.com>
2020-06-03 13:40:53 +0300

    add models.py

---
+++ app/itog_nagruzka_app/models.py
@@ -0,0 +1,50 @@
+import os
+
+from peewee import SqliteDatabase, Model, PrimaryKeyField, ForeignKeyField, CharField, IntegerField
+
+db = SqliteDatabase(os.environ.get('DB_FILE_PATH'))
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class StudyLevels(BaseModel):
+    name = CharField(max_length=255, unique=True)
+
+
+class Parallels(BaseModel):
+    name = IntegerField()
+    level = ForeignKeyField(StudyLevels, backref="level_parallels")
+
+
+class Classes(BaseModel):
+    id = PrimaryKeyField()
+    name = CharField(max_length=255)
+    parallel = ForeignKeyField(Parallels.name, backref="parallel_classes")
+    students_num = IntegerField()
+
+
+class Subjects(BaseModel):
+    name = CharField()
+
+
+class Stuff(BaseModel):
+    id = PrimaryKeyField()
+    name = CharField()
+    # position = CharField()
+
+
+class Metagroups(BaseModel):
+    id = PrimaryKeyField()
+    class_id = ForeignKeyField(Classes, backref="class_metagroups")
+    subject_name = ForeignKeyField(Subjects, backref="subject_metagroups")
+    meta_name = CharField(max_length=255)
+
+
+class Nagruzka(BaseModel):
+    id = PrimaryKeyField()
+    subject_name = ForeignKeyField(Subjects, backref="subject_metagrops")
+    stuff_id = ForeignKeyField(Stuff, backref="stuff_nagruzka")
+    meta_name = ForeignKeyField(Metagroups, backref="meta_nagruzka")
